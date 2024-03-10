179ff982700dddc8115937963c92db01abbbb8cd
Evan <53529192+BruceMcRooster@users.noreply.github.com>
2024-03-09 19:41:29 -0500

    Add close note pickup autos

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -5,10 +5,13 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.util.Units;
+import frc.robot.Constants;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.LauncherSubsystem;
 
 
 public final class Autos {
@@ -16,7 +19,16 @@ public final class Autos {
     public static Command exampleAuto(ExampleSubsystem subsystem) {
         return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
     }
-    
+
+    public static Command pickupCommand(IntakeSubsystem intakeSubsystem, LauncherSubsystem launcherSubsystem) {
+        return Commands.run(() -> intakeSubsystem.setPower(Constants.IntakeConstants.intakePower), intakeSubsystem).withTimeout(1.5)
+                .andThen(Commands.run(() -> intakeSubsystem.setPower(-Constants.IntakeConstants.intakePower))
+                        .alongWith(Commands.run(() -> launcherSubsystem.runLauncher(-0.1))).withTimeout(0.15))
+                .finallyDo(() -> {
+                    intakeSubsystem.setPower(0);
+                    launcherSubsystem.stopLauncher();
+                });
+    }
     
     private Autos()
     {
